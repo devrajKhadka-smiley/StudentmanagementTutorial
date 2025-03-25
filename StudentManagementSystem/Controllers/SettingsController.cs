69328cca463de3ddf7c01f562802aafa61725f71
Dev Raj Khadka <devrajkhadka941@gmail.com>
2025-03-25 07:51:02 +0545

    Add project files.

---
+++ StudentManagementSystem/Controllers/SettingsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+        [HttpGet]
+        public string? GetAIFromSetting()
+        {
+            //it gives the object of the iconfiguration by building as it should be empty in beginning
+            //IConfiguration config = new ConfigurationBuilder().Build();
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            string? aiChoosen = config["UsedAi"];
+            return aiChoosen;
+        }
+    }
+}
---
+++ StudentManagementSystem/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentManagementSystem.Models
+{
+    public class Student
+    {
+        public required string Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+        public string? Course { get; set; }
+
+    }
+}
